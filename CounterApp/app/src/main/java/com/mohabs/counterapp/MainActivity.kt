9c53ef40159ca16d0cabdb200705e64583d97d08
Mohab Yasser <133398078+MohabYasser2@users.noreply.github.com>
2024-07-23 12:06:59 +0300

    Add files via upload

---
+++ CounterApp/app/src/main/java/com/mohabs/counterapp/MainActivity.kt
@@ -0,0 +1,61 @@
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.maad.counterapp.ui.theme.CounterAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState) enableEdgeToEdge () setContent {
+            CounterAppTheme {
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding -> CounterApp(Modifier.padding(innerPadding)) }
+            }
+        }
+    }
+}
+
+@Composable
+fun CounterApp(modifier: Modifier = Modifier) {
+    var counter by remember { mutableIntStateOf(0) } Column (verticalArrangement =
+        Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally, modifier = modifier.fillMaxSize()) {
+        Text(
+            text = counter.toString(),
+            fontSize = 60.sp,
+            style = TextStyle(shadow = Shadow(Color.Blue, Offset(5.0f, 10.0f), 3f))
+        ) Button (onClick =
+            { ++counter }, colors = ButtonDefaults.buttonColors(containerColor = Color.Blue)) {
+        Text(
+            text = "Increment"
+        )
+    }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun CounterAppPreview() {
+    CounterApp()
+}
